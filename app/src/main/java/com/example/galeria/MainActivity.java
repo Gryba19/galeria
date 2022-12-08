53783f7ce91baed0fbff89475e16dd2c805f5cfb
LENOVO <gryba@zspswiecie.pl>
2022-12-08 22:30:57 +0100

    changes

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.galeria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -25,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
        // simpleGallery=findViewById(R.id.simpleGallery2);
         ImageView = findViewById(R.id.ImageView);
+        ImageView.setBackgroundResource(R.drawable.kot1);
+        tlo=findViewById(R.id.kolor_tla);
+        tlo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked==true)
+                {
+                    tlo.setBackgroundColor(Color.BLUE);
+                }
+                else
+                {
+                    tlo.setBackgroundColor(Color.GREEN);
+                }
+            }
+        });
         for(int i: images){
 
             ImageView imageView=new ImageView(this);

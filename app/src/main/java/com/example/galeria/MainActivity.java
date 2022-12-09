601fdb65a98020bc9c59f87b2d4648119a05f776
4THP <gryba@zspswiecie,pl>
2022-12-09 09:28:34 +0100

    a

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.galeria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Button prev;
     Switch tlo;
     EditText kotid;
+    int a=1;
+
     int[] images = {R.drawable.kot1, R.drawable.kot2, R.drawable.kot3, R.drawable.kot4,};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +31,82 @@ public class MainActivity extends AppCompatActivity {
        // simpleGallery=findViewById(R.id.simpleGallery2);
         ImageView = findViewById(R.id.ImageView);
         ImageView.setBackgroundResource(R.drawable.kot1);
+        next=findViewById(R.id.next);
+        kotid=findViewById(R.id.id_kot);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(a==1) {
+                    ImageView.setBackgroundResource(R.drawable.kot2);
+                    a=2;
+                }
+                else if(a==2) {
+                    ImageView.setBackgroundResource(R.drawable.kot3);
+                    a=3;
+                }
+                if(a==3) {
+                    ImageView.setBackgroundResource(R.drawable.kot4);
+                    a=4;
+                }
+                if(a==4) {
+                    ImageView.setBackgroundResource(R.drawable.kot1);
+                    a=1;
+                }
+            }
+        });
+        /*
+        int id=Integer.parseInt(kotid.getText().toString());
+        if(id==1)
+        {
+            ImageView.setBackgroundResource(R.drawable.kot1);
+        }
+        if(id==2)
+        {
+            ImageView.setBackgroundResource(R.drawable.kot2);
+        }
+        if(id==3)
+        {
+            ImageView.setBackgroundResource(R.drawable.kot3);
+        }
+        if(id==4)
+        {
+            ImageView.setBackgroundResource(R.drawable.kot4);
+        }*/
+        prev=findViewById(R.id.prev);
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(a==1) {
+                    ImageView.setBackgroundResource(R.drawable.kot4);
+                    a=4;
+                }
+                else if(a==2) {
+                    ImageView.setBackgroundResource(R.drawable.kot1);
+                    a=1;
+                }
+                if(a==3) {
+                    ImageView.setBackgroundResource(R.drawable.kot2);
+                    a=2;
+                }
+                if(a==4) {
+                    ImageView.setBackgroundResource(R.drawable.kot3);
+                    a=3;
+                }
+            }
+        });
         tlo=findViewById(R.id.kolor_tla);
+        ConstraintLayout bgElement = (ConstraintLayout) findViewById(R.id.abc);
         tlo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if(isChecked==true)
                 {
-                    tlo.setBackgroundColor(Color.BLUE);
+                    bgElement.setBackgroundColor(Color.rgb(17, 107, 191));
                 }
                 else
                 {
-                    tlo.setBackgroundColor(Color.GREEN);
+                    bgElement.setBackgroundColor(Color.rgb(3, 110, 7));
                 }
             }
         });
@@ -54,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             });
-            simpleGallery.addView(imageView);
+           // simpleGallery.addView(imageView);
         }
     }
 }

390ca5f4f6944caa378b3071693ad64af73e2971
4THP <gryba@zspswiecie.pl>
2022-12-12 09:14:00 +0100

    zad

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -16,7 +16,40 @@ import android.widget.LinearLayout;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    /********************************************************
+     nazwa : next
 
+     parametry wejściowe: a - liczba, która jest przypisana do danego zdjęcia
+     wartość zwracana: wartość liczby a i zdjęcie kota od 1-4
+     opis funkcji: gdy button next zostanie kliknięty funkcja sprawdzi wartość a i wyświeśli
+     następne zdjęcie z odpowiednim numerem potem zwiększając liczbe a o 1, ale gdy liczba a
+     wyniesie więcej niż cztery, funkcja zmieni wartość liczba a na 1.
+
+     nazwa: prev
+
+     parametry wejściowe: a - liczba, która jest przypisana do danego zdjęcia
+     wartość zwracana: wartość liczby a i zdjęcie kota od 1-4
+     opis funkcji: gdy button prev zostanie kliknięty funkcja sprawdzi wartość a i wyświeśli
+     poprzednie zdjęcie z odpowiednim numerem potem zmniejszy liczbe a o 1, ale gdy liczba a
+     wyniesie mniej niż 1, funkcja zmieni wartość liczba a na 4.
+
+     nazwa: kotid
+
+     parametry wejściowe: liczby wpisane z klawiatury
+     wartość zwracana: zdjęcia kota od 1-4
+     opis funkcji: po wpisani liczby od 1 do 4 wyświetli się obraz o odpowiednim
+     przypisanym numerze, a inne liczby będzie ignorował wyświetlając poprzedni obraz
+
+     nazwa:tlo
+
+     parametry wejściowe: nie zaznaczony przycisk switch
+     wartość zwracana: kolor tła zielony lub niebieski
+     opis funkcji: Nie zaznaczony przycisk switch sprawia że kolor tła aplikacji jest zielony,
+     ale po zaznaczeniu zmienia na kolor niebieski. Proces ten jest można cofnąć
+     klikając przycisk switch i kolor ponownie będzie zielony.
+
+      autor: Pesel
+     ****************************************************/
     LinearLayout simpleGallery;
     ImageView ImageView;
     Button next;
@@ -24,13 +57,11 @@ public class MainActivity extends AppCompatActivity {
     Switch tlo;
     EditText kotid;
     int a=1;
-
     int[] images = {R.drawable.kot1, R.drawable.kot2, R.drawable.kot3, R.drawable.kot4,};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // simpleGallery=findViewById(R.id.simpleGallery2);
         ImageView = findViewById(R.id.ImageView);
         ImageView.setBackgroundResource(R.drawable.kot1);
         next=findViewById(R.id.next);
@@ -69,11 +100,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
                 int id=Integer.parseInt(kotid.getText().toString());
                 if(id==1)
                 {
@@ -94,6 +120,11 @@ public class MainActivity extends AppCompatActivity {
                 else{
 
                 }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         };
         kotid.addTextChangedListener(textWatcher);
@@ -136,19 +167,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        for(int i: images){
-
-            ImageView imageView=new ImageView(this);
-            imageView.setImageResource(i);
-            imageView.setLayoutParams(new LinearLayout.LayoutParams(300,300));
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ImageView.setImageDrawable(((ImageView)v).getDrawable());
-
-                }
-            });
-           // simpleGallery.addView(imageView);
-        }
     }
 }
